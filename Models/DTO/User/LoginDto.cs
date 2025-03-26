c834a3d26255fa356d286647eb2646fb2ada392a
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-03-26 19:02:57 +0330

    update validation on data anotation  for register login & forgetpassword

--- Models/DTO/User/LoginDto.cs
+++ Models/DTO/User/LoginDto.cs
@@ -4,29 +4,31 @@ namespace SlideCloud.Models.DTO.User
 {
     public class RegisterDto
     {
-        [Required]
+        [Required(ErrorMessage ="نام را واردکنید")]
         public string FullName { get; set; }
         [Required]
-        [EmailAddress]
+        [EmailAddress(ErrorMessage ="فرمت ایمیل درست نیست")]
         public string Email { get; set; }
-        [Required]
-        [MinLength(4)]
+        [Required(ErrorMessage ="رمز خود را وارد کن")]
+        [MinLength(4,ErrorMessage ="حداقل باید 4 کلمه باشد")]
         public string Password { get; set; }
         [Required]
-        [Phone]
+        [RegularExpression(@"^09\d{9}$", ErrorMessage = "لطفاً یک شماره تلفن همراه معتبر ایرانی وارد کنید.")]
         public string PhoneNumber { get; set; }
     }
 
     public class LoginDto
     {
-        [Required]
+        [EmailAddress(ErrorMessage = "فرمت ایمیل درست نیست")]
+        [Required(ErrorMessage ="ایمیل خود را وارد کنید")]
         public string Email { get; set; }
-        [Required]
+        [Required(ErrorMessage = "رمز خود را وارد کن")]
         public string Password { get; set; }
     }
     public class ForgetPasswordDto
     {
-        [Required]
+        [EmailAddress(ErrorMessage = "فرمت ایمیل درست نیست")]
+        [Required(ErrorMessage = "ایمیل خود را وارد کنید")]
         public string Email { get; set; }
  
     }
