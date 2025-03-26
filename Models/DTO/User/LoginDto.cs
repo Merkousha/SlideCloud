feb6b002915d79690ce29a5cab5cb8579c2b4130
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-03-26 19:29:30 +0330

    verify email and phone exists for register

--- Models/DTO/User/LoginDto.cs
+++ Models/DTO/User/LoginDto.cs
@@ -1,18 +1,50 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using SlideCloud.Controller;
 
 namespace SlideCloud.Models.DTO.User
 {
+    using Microsoft.AspNetCore.Identity;
+    using System.ComponentModel.DataAnnotations;
+
+    public class RegisterModel
+    {
+        [Required(ErrorMessage = "وارد کردن رمز عبور الزامی است.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "تکرار رمز عبور الزامی است.")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "رمز عبور و تکرار آن باید یکسان باشند.")]
+        public string ConfirmPassword { get; set; }
+    }
     public class RegisterDto
     {
+     
+        [RegularExpression(@"^[\u0600-\u06FFa-zA-Z\s]+$", ErrorMessage = "نام باید فقط شامل حروف فارسی یا انگلیسی باشد.")]
+
         [Required(ErrorMessage ="نام را واردکنید")]
         public string FullName { get; set; }
-        [Required]
+
+
+        [Required(ErrorMessage = "ایمیل خود را وارد کنید")]
         [EmailAddress(ErrorMessage ="فرمت ایمیل درست نیست")]
         public string Email { get; set; }
+
+
+        [DataType(DataType.Password)]
         [Required(ErrorMessage ="رمز خود را وارد کن")]
         [MinLength(4,ErrorMessage ="حداقل باید 4 کلمه باشد")]
         public string Password { get; set; }
-        [Required]
+
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "رمز خود را وارد کن")]
+        [MinLength(4, ErrorMessage = "حداقل باید 4 کلمه باشد")]
+        [Compare(nameof(Password), ErrorMessage = "رمز عبور و تکرار آن باید یکسان باشند.")]
+        public string RePassword { get; set; }
+
+        [Required(ErrorMessage = "شماره خود را وارد کن")]
+
         [RegularExpression(@"^09\d{9}$", ErrorMessage = "لطفاً یک شماره تلفن همراه معتبر ایرانی وارد کنید.")]
         public string PhoneNumber { get; set; }
     }
