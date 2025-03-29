3e1f98ebae5db268a3f76cdb7f3f8a2f7d7708b9
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-03-28 17:31:48 -0800

    add Contact model to save comment in database

---
+++ Models/ContactUs/Contact.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SlideCloud.Models.ContactUs
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "این فیلد اجباریست")]
+        [MinLength(3, ErrorMessage = "حداقل طول نام 3 کاراکتر است.")]
+        [MaxLength(50, ErrorMessage = "حداکثر طول نام 50 کاراکتر است")]
+        public string FullName { get; set; }
+
+        [Required(ErrorMessage = "این فیلد اجباریست")]
+        [EmailAddress(ErrorMessage = "آدرس ایمیل وارد شده صحیح نمی باشد.")]
+        public string Email { get; set; }
+
+        [Phone(ErrorMessage = "شماره تماس وارد شده صحیح نمی باشد.")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = ".متن پیام نمیتواند خالی باشد")]
+        public string Message { get; set; }
+    }
+}
