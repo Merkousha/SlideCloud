1f4395d7ab2013746ad644ce71600f3fb4589a94
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-03-28 19:01:54 -0800

    edit data annotation in Contact model

--- Models/ContactUs/Contact.cs
+++ Models/ContactUs/Contact.cs
@@ -1,23 +1,40 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace SlideCloud.Models.ContactUs
 {
     public class Contact
     {
         public int Id { get; set; }
+        [DisplayName("نام و نام خانوادگی")]
         [Required(ErrorMessage = "این فیلد اجباریست")]
         [MinLength(3, ErrorMessage = "حداقل طول نام 3 کاراکتر است.")]
         [MaxLength(50, ErrorMessage = "حداکثر طول نام 50 کاراکتر است")]
         public string FullName { get; set; }
 
+        [DisplayName("ایمیل")]
         [Required(ErrorMessage = "این فیلد اجباریست")]
         [EmailAddress(ErrorMessage = "آدرس ایمیل وارد شده صحیح نمی باشد.")]
         public string Email { get; set; }
 
+        [DisplayName("شماره تماس")]
         [Phone(ErrorMessage = "شماره تماس وارد شده صحیح نمی باشد.")]
         public string PhoneNumber { get; set; }
 
+        [DisplayName("متن پبام")]
         [Required(ErrorMessage = ".متن پیام نمیتواند خالی باشد")]
         public string Message { get; set; }
+
+        public Contact() { }
+
+        public Contact(string fullName, string email, string phoneNumber, string message)
+        {
+            FullName = fullName;
+            Email = email;
+            PhoneNumber = phoneNumber;
+            Message = message;
+        }
     }
+
+
 }
