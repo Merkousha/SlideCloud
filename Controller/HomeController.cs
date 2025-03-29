8e41a1d051f6859012e933b3319477d3f899f4b9
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-03-28 19:00:05 -0800

    add contact Action

--- Controller/HomeController.cs
+++ Controller/HomeController.cs
@@ -1,10 +1,44 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AspNetCoreGeneratedDocument;
+using Microsoft.AspNetCore.Mvc;
+using SlideCloud.Data;
+using SlideCloud.Models.ContactUs;
 
 namespace SlideCloud.Controller;
 public class HomeController : Microsoft.AspNetCore.Mvc.Controller
 {
+
+    private readonly AppDbContext _context;
+
+    public HomeController(AppDbContext context)
+    {
+        _context = context;
+    }
+
     public IActionResult Index()
     {
+        return View();
+    }
+
+    [HttpGet]
+    public IActionResult Contact()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Contact(Contact model)
+    {
+        if (ModelState.IsValid)
+        {
+            var contact = new Contact(model.FullName, model.Email, model.PhoneNumber, model.Message);
+            _context.Contacts.Add(contact);
+            _context.SaveChanges();
+            TempData["Success"] = "پیام شما با موفقیت ارسال شد";
+        }
+        else
+        {
+            TempData["Error"] = "لطفا مقادیر خواسته شده را بطور صحیح وارد نمایید";
+        }
         return View();
     }
 }
