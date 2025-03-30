b4d3866b78910389860309c8ca8e95a84b25dea4
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-03-30 17:56:38 +0330

    add post action update and add View Update

--- Areas/User/Controllers/ManageSlideController.cs
+++ Areas/User/Controllers/ManageSlideController.cs
@@ -67,58 +67,60 @@ namespace SlideCloud.Areas.User.Controllers
             {
                 return NotFound();
             }
-       
-                //check if the user is the owner of the document
-                //if()
-                var documnet=await _appDbContext.Documents.FindAsync(id);
-                if(documnet != null)
-                {
-                    var model = new UpdateSlides();
 
-                    model.Id = documnet.Id;
-                    model.Description = documnet.Description;
-                    model.DocumentCategoryId = documnet.DocumentCategoryId;
-                    model.DocumentTypeId = documnet.DocumentTypeId;
-                    model.File = documnet.File;
-                    model.Picture = documnet.Picture;
-                    model.Title = documnet.Title;
-                    model.ViewCount = model.ViewCount;
-                    ViewBag.DocumentTypes = _appDbContext.DocumentTypes.ToList();
-                    ViewBag.DocumentCategories = _appDbContext.DocumentCategories.ToList();
-                    return View(model);
-                }
-             
-                return NotFound();
+            //check if the user is the owner of the document
+            //if()
+            var documnet = await _appDbContext.Documents.FindAsync(id);
+            if (documnet != null)
+            {
+                var model = new UpdateSlides();
+
+                model.Id = documnet.Id;
+                model.Description = documnet.Description;
+                model.DocumentCategoryId = documnet.DocumentCategoryId;
+                model.DocumentTypeId = documnet.DocumentTypeId;
+                model.File = documnet.File;
+                model.Picture = documnet.Picture;
+                model.Title = documnet.Title;
+                model.ViewCount = model.ViewCount;
+                ViewBag.DocumentTypes = _appDbContext.DocumentTypes.ToList();
+                ViewBag.DocumentCategories = _appDbContext.DocumentCategories.ToList();
+                return View(model);
+            }
+
+            return NotFound();
 
 
         }
 
         [HttpPost]
-        public IActionResult Update(CreateSlides model)
+        public async Task<IActionResult> Update(UpdateSlides model)
         {
             ViewBag.DocumentTypes = _appDbContext.DocumentTypes.ToList();
             ViewBag.DocumentCategories = _appDbContext.DocumentCategories.ToList();
-            if (!ModelState.IsValid)
-            {
+            //check if the user is the owner of the document
+            //if()
 
+            if (!ModelState.IsValid)
                 return View(model);
 
-            }
-            _appDbContext.Documents.Add(new SlideCloud.Models.Document
+            var documnet = await _appDbContext.Documents.FindAsync(model.Id);
+            if (documnet != null)
             {
-                Description = model.Description,
-                DocumentCategoryId = model.DocumentCategoryId,
-                DocumentTypeId = model.DocumentTypeId,
-                File = model.File,
-                Picture = model.Picture,
-                Title = model.Title,
-                ViewCount = 0
 
-            });
-            _appDbContext.SaveChanges();
+                documnet.Description = model.Description;
+                documnet.DocumentCategoryId = model.DocumentCategoryId;
+                documnet.DocumentTypeId = model.DocumentTypeId;
+                documnet.File = model.File;
+                documnet.Picture = model.Picture;
+                documnet.Title = model.Title;
+                _appDbContext.Documents.Update(documnet);
+                await _appDbContext.SaveChangesAsync();
 
-            //return  RedirectToAction("Index", "Home");
-            return Redirect("~/");
+                return RedirectToAction("Detail", "Slide", new {id=model.Id});
+            }
+            return View(model);
+           
         }
 
 
