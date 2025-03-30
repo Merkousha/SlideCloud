c2279fb0d9e72451c19fde2f4630fe9285df427d
Merkousha <merkousha.net@gmail.com>
2025-03-30 12:46:28 +0330

    Fix Slide's List and detail Routing

--- Areas/User/Controllers/ManageSlideController.cs
+++ Areas/User/Controllers/ManageSlideController.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using SlideCloud.Areas.User.Models.Slides;
 using SlideCloud.Data;
 
@@ -12,47 +11,17 @@ namespace SlideCloud.Areas.User.Controllers
     {
         private readonly AppDbContext _appDbContext;
 
-        public ManageSlideController(AppDbContext appDbContext) 
+        public ManageSlideController(AppDbContext appDbContext)
         {
             _appDbContext = appDbContext;
 
         }
-        #region List Of Sldie
-
-
-        [HttpGet("/User/[Controller]/[Action]",Name ="ListSlideForUser")]
-        public async Task<IActionResult> List()
-        {
-            var model = new ListSlideVM();
-            model.DocumentCategories = await _appDbContext.DocumentCategories.ToListAsync();
-            model.Documents = await _appDbContext.Documents.ToListAsync();
-            return View(model);
-        }
-
-
-        #endregion
-        [HttpGet]
-
-        [HttpGet("/User/ManageSlide/DetailsOfList", Name = "DetailsSlide")]
-        public async Task<IActionResult> DetailsOfList(int id)
-        {
-            var model = new DetailsSlideVM();
-            model.DocumentDetail = await _appDbContext.Documents
-                .Include(a => a.DocumentCategory)
-                .FirstOrDefaultAsync(a => a.Id == id);
-            if (model.DocumentDetail == null)
-            {
-                return NotFound();
-            }
-            return View(model);
-        }
-        
 
 
         [HttpGet]
         public IActionResult Create()
         {
-           
+
             var model = new CreateSlides();
             ViewBag.DocumentTypes = _appDbContext.DocumentTypes.ToList();
             ViewBag.DocumentCategories = _appDbContext.DocumentCategories.ToList();
@@ -66,7 +35,7 @@ namespace SlideCloud.Areas.User.Controllers
             ViewBag.DocumentCategories = _appDbContext.DocumentCategories.ToList();
             if (!ModelState.IsValid)
             {
-                
+
                 return View(model);
 
             }
---
+++ Controller/SlideController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SlideCloud.Areas.User.Models.Slides;
+using SlideCloud.Data;
+
+namespace SlideCloud.Controller;
+public class SlideController : Microsoft.AspNetCore.Mvc.Controller
+{
+    private readonly AppDbContext _appDbContext;
+
+    public SlideController(AppDbContext appDbContext)
+    {
+        _appDbContext = appDbContext;
+
+    }
+
+
+    #region List Of Sldie
+    public async Task<IActionResult> Index()
+    {
+        var model = new ListSlideVM();
+        model.DocumentCategories = await _appDbContext.DocumentCategories.ToListAsync();
+        model.Documents = await _appDbContext.Documents.ToListAsync();
+        return View(model);
+    }
+    #endregion
+
+    [HttpGet]
+    public async Task<IActionResult> Detail(int id)
+    {
+        var model = new DetailsSlideVM();
+        model.DocumentDetail = await _appDbContext.Documents
+            .Include(a => a.DocumentCategory)
+            .FirstOrDefaultAsync(a => a.Id == id);
+        if (model.DocumentDetail == null)
+        {
+            return NotFound();
+        }
+        return View(model);
+    }
+}
