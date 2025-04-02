269b751a46571f8c1ebac717f7faaaec779aecba
mohammad mehdi khodabande <mmahdikhodabande@gmail.com>
2025-04-02 20:35:25 +0330

    Add a list of users for admins to manage

--- Areas/Admin/Controllers/ManageSlideController.cs
+++ Areas/Admin/Controllers/ManageSlideController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using SlideCloud.Areas.Admin.Models.Slides;
 using SlideCloud.Data;
@@ -6,21 +7,36 @@ using SlideCloud.Data;
 namespace SlideCloud.Areas.Admin.Controllers
 {
     [Area("Admin")]
-    [Authorize(Roles ="Admin")]
+    [Authorize(Roles = "Admin")]
     public class ManageSlideController : Microsoft.AspNetCore.Mvc.Controller
     {
+        private readonly UserManager<SlideCloud.Models.User> _userManager;
         private readonly AppDbContext _appDbContext;
 
-        public ManageSlideController(AppDbContext appDbContext) 
+        public ManageSlideController(UserManager<SlideCloud.Models.User> userManager, AppDbContext appDbContext)
         {
+            _userManager = userManager;
             _appDbContext = appDbContext;
-
         }
 
+        public async Task<IActionResult> Index()
+        {
+            var users = _userManager.Users.ToList();
+            var userRoles = new Dictionary<long, List<string>>();
+
+            foreach (var user in users)
+            {
+                var roles = await _userManager.GetRolesAsync(user);
+                userRoles[user.Id] = roles.ToList();
+            }
+
+            ViewBag.UserRoles = userRoles;
+            return View(users);
+        }
         [HttpGet]
         public IActionResult Create()
         {
-           
+
             var model = new CreateSlides();
             ViewBag.DocumentTypes = _appDbContext.DocumentTypes.ToList();
             ViewBag.DocumentCategories = _appDbContext.DocumentCategories.ToList();
@@ -34,7 +50,7 @@ namespace SlideCloud.Areas.Admin.Controllers
             ViewBag.DocumentCategories = _appDbContext.DocumentCategories.ToList();
             if (!ModelState.IsValid)
             {
-                
+
                 return View(model);
 
             }
@@ -50,11 +66,32 @@ namespace SlideCloud.Areas.Admin.Controllers
 
             });
             _appDbContext.SaveChanges();
-            return RedirectToAction("Index","Home");
+            return RedirectToAction("Index", "Home");
         }
 
 
+        public async Task<IActionResult> PromoteToAdmin(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null) return NotFound();
 
+            await _userManager.AddToRoleAsync(user, "Admin");
+
+            string url = "/Admin/ManageSlide/Index";
+
+            return Redirect(url);
+        }
+
+        public async Task<IActionResult> DemoteToUser(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null) return NotFound();
+
+            await _userManager.RemoveFromRoleAsync(user, "Admin");
+
+            string url = "/Admin/ManageSlide/Index";
+            return Redirect(url);
+        }
 
     }
 }
