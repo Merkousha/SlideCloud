a6f7f0f7c563c51ff009b97a9e006053c9ff8854
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-04-02 16:05:57 +0330

    Add Model CreateCategoryVM For Create

---
+++ Areas/Admin/Models/Category/CreateCategoryVM.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SlideCloud.Areas.Admin.Models.Category
+{
+    public class CreateCategoryVM
+    {
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [MaxLength(255)]
+        public string Description { get; set; }
+
+        [MaxLength(50)]
+        public string Slug { get; set; }
+    }
+}
