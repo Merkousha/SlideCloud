885e30f1ef9ae7bca9f62b1f62e0a687e0dbb893
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-04-02 17:05:00 +0330

    Add Validation For Update And Create Model with DataAnotaion

--- Areas/Admin/Models/Category/CreateCategoryVM.cs
+++ Areas/Admin/Models/Category/CreateCategoryVM.cs
@@ -4,14 +4,18 @@ namespace SlideCloud.Areas.Admin.Models.Category
 {
     public class CreateCategoryVM
     {
-        [Required]
+
+        [Required(ErrorMessage = "{0} را وارد کنید")]
         [MaxLength(50)]
+        [Display(Name = "نام")]
         public string Name { get; set; }
-
+        [Required(ErrorMessage = "{0} را وارد کنید")]
         [MaxLength(255)]
+        [Display(Name = "توضیحات")]
         public string Description { get; set; }
-
+        [Required(ErrorMessage = "{0} را وارد کنید")]
         [MaxLength(50)]
+        [Display(Name = "عنوان در مرورگر")]
         public string Slug { get; set; }
     }
 }
--- Areas/Admin/Models/Category/UpdateCategoryVM.cs
+++ Areas/Admin/Models/Category/UpdateCategoryVM.cs
@@ -6,14 +6,17 @@ namespace SlideCloud.Areas.Admin.Models.Category
     {
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage ="{0} را وارد کنید")]
         [MaxLength(50)]
+        [Display(Name ="نام")]
         public string Name { get; set; }
-
+        [Required(ErrorMessage = "{0} را وارد کنید")]
         [MaxLength(255)]
+        [Display(Name = "توضیحات")]
         public string Description { get; set; }
-
+        [Required(ErrorMessage = "{0} را وارد کنید")]
         [MaxLength(50)]
+        [Display(Name = "عنوان در مرورگر")]
         public string Slug { get; set; }
     }
 }
