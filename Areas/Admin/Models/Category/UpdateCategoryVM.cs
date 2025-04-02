fdfea29d86e7266e94248d0700514688a2446cc1
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-04-02 16:37:16 +0330

    add Model UpdateCategoryVM For Update Category

---
+++ Areas/Admin/Models/Category/UpdateCategoryVM.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SlideCloud.Areas.Admin.Models.Category
+{
+    public class UpdateCategoryVM
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [MaxLength(255)]
+        public string Description { get; set; }
+
+        [MaxLength(50)]
+        public string Slug { get; set; }
+    }
+}
