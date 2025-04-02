db868aee9fca8fd27e53975bc6a981197d93afdd
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-04-02 15:35:38 +0330

    Add Controller Manage Category And Add Action List OF CategoryDocument

---
+++ Areas/Admin/Controllers/ManageCategoryController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SlideCloud.Data;
+
+namespace SlideCloud.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class ManageCategoryController : Microsoft.AspNetCore.Mvc.Controller
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public ManageCategoryController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+
+        }
+        public IActionResult List()
+        {
+
+            return View(_appDbContext.DocumentCategories.ToList());
+        }
+    }
+}
