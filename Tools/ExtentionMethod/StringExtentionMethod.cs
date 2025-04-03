4d860439266d00466dc47c54ff31cd48d6505ab9
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-04-03 16:11:54 +0330

    Add Extention Method GetTextBy20Length For Description

---
+++ Tools/ExtentionMethod/StringExtentionMethod.cs
@@ -0,0 +1,13 @@
+﻿
+    public static class StringExtentionMethod
+    {
+        public static string GetTextBy20Length(this string text) {
+
+            return text?.Length > 20
+                ? text?.Substring(0, 20) + "..."
+                : text;
+
+            
+        }
+    }
+
