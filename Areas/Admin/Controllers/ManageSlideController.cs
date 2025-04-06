af2f6f68549c509f7f78119599d5c054dab4cf87
tiyam <tiyambakhshian@gmail.com>
2025-04-06 23:23:12 +0330

    Modify ManageSlideController
    Create ManageSlidesForAdmin region :
    add Update /Delete/UploadSlide action
    modify Create Action
    Create List Of Sldie For Admin region :
    add ListOfSlide Action

--- Areas/Admin/Controllers/ManageSlideController.cs
+++ Areas/Admin/Controllers/ManageSlideController.cs
@@ -1,8 +1,13 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Drawing.Printing;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SlideCloud.Areas.Admin.Models.Slides;
 using SlideCloud.Data;
+using SlideCloud.Models;
+
 
 namespace SlideCloud.Areas.Admin.Controllers
 {
@@ -10,14 +15,187 @@ namespace SlideCloud.Areas.Admin.Controllers
     [Authorize(Roles = "Admin")]
     public class ManageSlideController : Microsoft.AspNetCore.Mvc.Controller
     {
-        private readonly UserManager<SlideCloud.Models.User> _userManager;
+        UserManager<SlideCloud.Models.User> _userManager;
         private readonly AppDbContext _appDbContext;
+        private readonly IConfiguration _configuration;
+        private readonly IS3Uploader _s3Uploader;
 
-        public ManageSlideController(UserManager<SlideCloud.Models.User> userManager, AppDbContext appDbContext)
+        public ManageSlideController(AppDbContext appDbContext, IConfiguration configuration, IS3Uploader s3Uploader, UserManager<SlideCloud.Models.User> userManager)
         {
-            _userManager = userManager;
             _appDbContext = appDbContext;
+            _configuration = configuration;
+            _s3Uploader = s3Uploader;
+            _userManager = userManager;
         }
+
+        #region List Of Sldie For Admin
+        public async Task<IActionResult> ListOfSlide()
+        {
+            var query = _appDbContext.Documents.OrderBy(u => u.Id).ToList();
+            return View(query);
+        }
+        #endregion
+
+
+        #region ManageSlidesForAdmin
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewBag.DocumentTypes = _appDbContext.DocumentTypes.ToList();
+            ViewBag.DocumentCategories = _appDbContext.DocumentCategories.ToList();
+
+            var model = new CreateSlides();
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateSlides model)
+        {
+            ViewBag.DocumentTypes = _appDbContext.DocumentTypes.ToList();
+            ViewBag.DocumentCategories = _appDbContext.DocumentCategories.ToList();
+
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = await _userManager.Users.Where(a => a.Email.Equals(User.Identity.Name)).FirstOrDefaultAsync();
+            _appDbContext.Documents.Add(new Document
+            {
+                Title = model.Title,
+                Description = model.Description,
+                File = model.File,
+                Picture = model.Picture,
+                DocumentTypeId = model.DocumentTypeId,
+                DocumentCategoryId = model.DocumentCategoryId,
+                UserId = user.Id,
+                ViewCount = 0
+            });
+
+            await _appDbContext.SaveChangesAsync();
+            return Redirect("/Admin/ManageSlide/ListOfSlide");
+           // return RedirectToAction("ListOfSlide", "ManageSlide");
+            
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> UploadSlide(IFormFile file, string type)
+        {
+            try
+            {
+                if (file == null || file.Length == 0)
+                    return BadRequest(new { message = "فایلی دریافت نشد" });
+
+                // بررسی نوع فایل
+                if (type == "picture" && !file.ContentType.StartsWith("image/"))
+                    return BadRequest(new { message = "فایل انتخاب شده باید تصویر باشد" });
+
+                if (type == "file" && !IsValidSlideFile(file))
+                    return BadRequest(new { message = "فایل باید از نوع PDF یا PowerPoint باشد" });
+
+                var fileUrl = await _s3Uploader.UploadFileAsync(file);
+                return Ok(new { filePath = fileUrl });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = "خطا در آپلود فایل: " + ex.Message });
+            }
+        }
+
+        private bool IsValidSlideFile(IFormFile file)
+        {
+            var validExtensions = new[] { ".pdf", ".ppt", ".pptx" };
+            var extension = Path.GetExtension(file.FileName).ToLowerInvariant();
+            return validExtensions.Contains(extension);
+        }
+
+
+
+        [HttpGet]
+        public async Task<IActionResult> Update(int id)
+        {
+
+            var documnet = await _appDbContext.Documents.FindAsync(id);
+
+            if (documnet != null)
+            {
+                var model = new UpdateSlides();
+
+                model.Id = documnet.Id;
+                model.Description = documnet.Description;
+                model.DocumentCategoryId = documnet.DocumentCategoryId;
+                model.DocumentTypeId = documnet.DocumentTypeId;
+                model.File = documnet.File;
+                model.Picture = documnet.Picture;
+                model.Title = documnet.Title;
+                model.ViewCount = model.ViewCount;
+                ViewBag.DocumentTypes = _appDbContext.DocumentTypes.ToList();
+                ViewBag.DocumentCategories = _appDbContext.DocumentCategories.ToList();
+                return View(model);
+            }
+
+            return NotFound();
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(UpdateSlides model)
+        {
+            ViewBag.DocumentTypes = _appDbContext.DocumentTypes.ToList();
+            ViewBag.DocumentCategories = _appDbContext.DocumentCategories.ToList();
+
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var documnet = await _appDbContext.Documents.FindAsync(model.Id);
+
+            var user = await _userManager.Users.Where(a => a.Email.Equals(User.Identity.Name)).FirstOrDefaultAsync();
+            if (user != null)
+            {
+                if (documnet != null)
+                {
+                    documnet.Description = model.Description;
+                    documnet.DocumentCategoryId = model.DocumentCategoryId;
+                    documnet.DocumentTypeId = model.DocumentTypeId;
+                    documnet.File = model.File;
+                    documnet.Picture = model.Picture;
+                    documnet.Title = model.Title;
+                    documnet.UserId = user.Id;
+                    _appDbContext.Documents.Update(documnet);
+                    await _appDbContext.SaveChangesAsync();
+
+                    return RedirectToAction("ListOfSlide", "ManageSlide", new { id = model.Id });
+                }
+
+            }
+            return View(model);
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+
+            var Document = await _appDbContext.Documents.FirstOrDefaultAsync(a => a.Id == id);
+
+            if (Document == null)
+            {
+                return NotFound();
+            }
+
+                _appDbContext.Documents.Remove(Document);
+                await _appDbContext.SaveChangesAsync();
+                return Redirect("~/Admin/ManageSlide/ListOfSlide");
+       
+
+        }
+
+        #endregion
+
+
 
         public async Task<IActionResult> Index()
         {
@@ -33,41 +211,7 @@ namespace SlideCloud.Areas.Admin.Controllers
             ViewBag.UserRoles = userRoles;
             return View(users);
         }
-        [HttpGet]
-        public IActionResult Create()
-        {
 
-            var model = new CreateSlides();
-            ViewBag.DocumentTypes = _appDbContext.DocumentTypes.ToList();
-            ViewBag.DocumentCategories = _appDbContext.DocumentCategories.ToList();
-            return View(model);
-        }
-
-        [HttpPost]
-        public IActionResult Create(CreateSlides model)
-        {
-            ViewBag.DocumentTypes = _appDbContext.DocumentTypes.ToList();
-            ViewBag.DocumentCategories = _appDbContext.DocumentCategories.ToList();
-            if (!ModelState.IsValid)
-            {
-
-                return View(model);
-
-            }
-            _appDbContext.Documents.Add(new SlideCloud.Models.Document
-            {
-                Description = model.Description,
-                DocumentCategoryId = model.DocumentCategoryId,
-                DocumentTypeId = model.DocumentTypeId,
-                File = model.File,
-                Picture = model.Picture,
-                Title = model.Title,
-                ViewCount = 0
-
-            });
-            _appDbContext.SaveChanges();
-            return RedirectToAction("Index", "Home");
-        }
 
 
         public async Task<IActionResult> PromoteToAdmin(string id)
